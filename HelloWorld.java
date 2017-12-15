4cfea036d329f3ef8c6c5ed5b56ac15d07222677
Bowei Feng <boweif@qti.qualcomm.com>
2017-12-15 15:05:24 -0800

    Adding HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        String s = new String("hello world!");
+        System.out.println(s);
+    }
+}
+
+ class Hello2
+{
+    public static void main(String[] args)
+    {
+        String s = new String("hello world 2!");
+        System.out.println(s);
+    }
+}
