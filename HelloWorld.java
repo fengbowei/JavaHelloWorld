8282e71be1786dfe50667ddd23101c4b75fc44f5
Bowei Feng <boweif@qti.qualcomm.com>
2017-12-16 15:56:18 -0800

    Add Timer Example and Lamda expression

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,58 @@
 import com.test.Lib;
 import com.test.Printer;
 import com.test.Printable;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
 
 public class HelloWorld
 {
     public static void main(String[] args)
     {
+        // 1
         String s = new String("hello world!");
         System.out.println(s);
+
+        //2
         Lib a = new Lib();
         a.sayHello();
 
+        //3
         HelloInterface b = new HelloInterface(1, "jack");
         System.out.println("calling printer...\n");
         Printer.print(b);
-        
+
+        //4
+
+        // 4.1 passing an implemation of interface
+        /*
+        ActionListener listener = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                System.out.println("HelloWorld From ActionListener!\n");
+            }
+        };
+        Timer t = new Timer(1000, listener);
+        */
+
+        //4.2 passing lamda expression(only on Java8)
+        /*
+        Timer t = new Timer(1000, (ActionEvent event)->
+        {
+            System.out.println("HelloWorld From ActionListener!\n");
+        });
+        t.start();
+        */
+        try
+        {
+            TimeUnit.SECONDS.sleep(5);   
+        }
+        catch(InterruptedException e)
+        {
+        }
+
+        t.stop();
     }
 }
 
